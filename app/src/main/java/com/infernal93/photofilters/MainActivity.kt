8500fab55996a3b678361ba740fb73658f25d6fa
armdev19 <armdev19@gmail.com>
2019-10-02 20:09:17 +0400

    Initial commit - Create Custom ViewPager - Create EditImage Fragment - Create FilterListFragment - Add first design

---
+++ app/src/main/java/com/infernal93/photofilters/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.infernal93.photofilters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/infernal93/photofilters/Utils/NonSwipeViewPager.kt
@@ -0,0 +1,47 @@
+package com.infernal93.photofilters.Utils
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.animation.DecelerateInterpolator
+import android.widget.Scroller
+import androidx.viewpager.widget.ViewPager
+import java.lang.Exception
+
+class NonSwipeViewPager: ViewPager {
+    private val TAG = "NonSwipeViewPager"
+
+    constructor(context: Context) : super(context) {
+
+        setMyScroller()
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        setMyScroller()
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        return false
+    }
+
+    private fun setMyScroller() {
+        try {
+            val viewPager = ViewPager::class.java
+            val scroller = viewPager.getDeclaredField("mScroller")
+            scroller.isAccessible = true
+            scroller.set(this, MyScroller(context))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
+
+class MyScroller(context: Context): Scroller(context, DecelerateInterpolator()) {
+    override fun startScroll(startX: Int, startY: Int, dx: Int, dy: Int, duration: Int) {
+        super.startScroll(startX, startY, dx, dy, 400)
+    }
+}
